9a567268e30837f7317a789918cfb6d943fc8c5a
Mikołaj <226109@student.pwr.edu.pl>
2018-05-27 23:03:59 +0100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='projectname',
+    version='1.0',
+    author='Your Name',
+    author_email='you@youraddress.com',
+    url='http://www.you.com/projectname',
+    packages=['projectname', 'projectname.utils'],
+)
