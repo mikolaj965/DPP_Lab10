cf128c42d92c557b6c065a09c07c9c04fd51a4b5
Mikołaj <226109@student.pwr.edu.pl>
2018-05-28 01:41:31 +0100

    updated functions

--- lab10/solv.py
+++ lab10/solv.py
@@ -1,13 +1,25 @@
 import math
 
-def getsolution1(a, b, c):
-    delta = b*b - 4*a*c
-    x1 = -b-math.sqrt(delta)/(2*a)
+delta = 0
 
+def calculatedelta(a, b, c):
+    delta = b*b - 4*a*c
+    return delta
+
+def getsolution1(a, b, c):
+    d = calculatedelta(a,b,c)
+    if d >= 0:
+        x1 = (-b-math.sqrt(d))/(2*a)
+    else:
+        return -1
     return x1
 
+
 def getsolution2(a, b, c):
-    delta = b*b - 4*a*c
-    x2 = -b+math.sqrt(delta)/(2*a)
+    d = calculatedelta(a,b,c)
+    if d >= 0:
+        x2 = (-b+math.sqrt(d))/(2*a)
+    else:
+        return -1
 
     return x2
